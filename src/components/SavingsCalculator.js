3cd1a7dcaab07dcbeea5cef2329aab581f3814f4
Kevin Morales <k@hidalgo.se>
2023-08-15 12:41:06 +0200

    added input fields and styling

--- src/components/SavingsCalculator.js
+++ src/components/SavingsCalculator.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './SavingsCalculator.css';
 
-// Här skapar vi vår huvudkomponent
 function SavingsCalculator() {
-    const [rates, setRates] = useState([]); // Denna state lagrar våra räntesatser
+    const [rates, setRates] = useState([]); 
+    const [deposit, setDeposit] = useState(0);
+    const [years, setYears] = useState(1);
 
-    // Denna funktion hämtar räntesatser från SBAB API
     useEffect(() => {
         fetch("https://developer.sbab.se/sandbox/api/interest-rates/2.1/deposit-rates")
         .then(response => response.json())
@@ -14,10 +14,39 @@ function SavingsCalculator() {
         });
     }, []);
 
+    // Här lagrar vi vårt resultat (sparande med ränta)
+    const [result, setResult] = useState(null);
+
+    const handleCalculate = () => {
+        // Exempel: Använd den första räntesatsen för att beräkna (du kan utvidga detta senare)
+        const interestRate = rates[0]; 
+        const calculatedValue = deposit * (1 + interestRate/100) ** years;
+        setResult(calculatedValue);
+    }
+
     return (
-        <div>
-            <h2>SBAB Sparande Kalkylator</h2>
-            {/* Vi kommer att behöva ett formulär här för att ta input från användaren och visa resultatet */}
+        <div className="calculator-container">
+            <h1>SBAB Sparande Kalkylator</h1>
+            <div className="input-section">
+                <label>
+                    Initial Insättning (SEK):
+                    <input type="number" value={deposit} onChange={(e) => setDeposit(parseFloat(e.target.value))} />
+                </label>
+                <label>
+                    Antal År:
+                    <input type="number" value={years} onChange={(e) => setYears(parseFloat(e.target.value))} />
+                </label>
+                <button onClick={handleCalculate}>Beräkna Sparande</button>
+            </div>
+            <div className="result-section">
+                {result && <p>Ditt sparande efter {years} år: {result.toFixed(2)} SEK</p>}
+            </div>
+            <div className="rates-section">
+                <h2>Räntesatser:</h2>
+                {rates.map((rate, index) => (
+                    <p key={index}>Ränta {index + 1}: {rate}%</p>
+                ))}
+            </div>
         </div>
     );
 }
